fc45413c0bcf58fc91a928fcc9c85e1875492c4e
mortiest-morty <mortyyy13@gmail.com>
2026-03-17 11:12:41 +0500

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,42 @@
+#pragma once
+#ifndef HEADER_H
+#define HEADER_H
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <vector>
+#include <deque>
+#include <list>
+#include <cctype>
+
+
+// проверка ввода
+std::string getText();
+int getNumber();
+
+
+// Ввод и вывод контейнера "Vector"
+void inputVector(std::vector<int>& V);
+void outputVector(std::vector<int>& V);
+
+// Ввод и вывод контейнера "List"
+void inputList(std::list<int>& L);
+void outputList(std::list<int>& L);
+
+// Ввод и вывод контейнера "Deque"
+void inputDeque(std::deque<int>& D);
+void outputDeque(std::deque<int>& D);
+
+// Ввод контейнеров: "Vector", "List", "Deque" - для задания 1
+void InputContainersForTask_1(std::vector<int>& V, std::deque<int>& D, std::list<int>& L);
+
+
+// Задачи
+void task_1();
+void task_2();
+void task_3();
+void task_4();
+
+
+#endif
